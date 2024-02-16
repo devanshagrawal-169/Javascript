06a3dec860f6e0bbf629727543ebda3e898e3202
devanshagrawal-169 <devanshagrawal169@gmail.com>
2024-02-17 03:24:16 +0530

    video_7

---
+++ Basics/operations.js
@@ -0,0 +1,24 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+// console.log(2 % 3)
+// console.log(2 ** 3)
+
+let str1 = "Devansh"
+let str2 = " Agrawal"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2")
+
+console.log(+true)
