cdebb6f9cd62454d35c076d01e8a817ace4e97bf
devanshagrawal-169 <devanshagrawal169@gmail.com>
2024-02-17 02:46:21 +0530

    video_5_datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,23 @@
+// use as strict version of JS code
+"use strict";
+
+// alert(3 + 3); we are uising NOde js, not a browser
+
+let name = "devansh";
+let age = 18
+let isLoggedin = false
+let state;
+
+let city = null
+
+// number -> 2 to power some number
+// string -> ""
+// boolean -> true, false
+// null -> standalone value
+// undefined 
+// symbol
+// object
+
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof(isLoggedin))
