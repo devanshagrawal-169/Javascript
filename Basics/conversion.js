5a5cf415d1aeb8fbf77b606a7de528faf408132e
devanshagrawal-169 <devanshagrawal169@gmail.com>
2024-02-17 03:06:07 +0530

    video_6_conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,34 @@
+let score = "22";
+
+console.log(typeof score)
+
+console.log(typeof(score))
+
+let inNumber = Number(score);
+console.log(inNumber);
+console.log(typeof(inNumber))
+
+
+// "33" -> 33
+// "223ab" -> NaN
+// true -> 1
+// false -> 0
+
+let isLoggedin = 1
+let inBoolean = Boolean(isLoggedin)
+
+console.log(inBoolean)
+console.log(typeof(inBoolean))
+
+// 1 -> true
+// 0 -> false
+// " " -> false
+// "devansh" -> true
+
+
+let Num = 20
+
+let stringNumber = String(Num);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber))
