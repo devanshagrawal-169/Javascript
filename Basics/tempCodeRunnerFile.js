05cbeaa2b7d630b33288f64cae8cdd4a2cc8645c
devanshagrawal-169 <devanshagrawal169@gmail.com>
2024-02-17 16:48:58 +0530

    video_13

--- Basics/tempCodeRunnerFile.js
+++ Basics/tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.3));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(9.9));
-console.log(Math.sqrt(100));
-console.log(Math.min(3, 4, 5, 9, 23));
-console.log(Math.max(3, 4, 5, 9, 23));
+let myDate = new Date();
